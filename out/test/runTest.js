dac63e1b8ac747469f37a7628ecf19c4f0449c61
viveksharmaui <sharma_vivek62@yahoo.com>
2019-09-28 02:24:27 +0500

    development env setup

---
+++ out/test/runTest.js
@@ -0,0 +1,40 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = __importStar(require("path"));
+const vscode_test_1 = require("vscode-test");
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            // The folder containing the Extension Manifest package.json
+            // Passed to `--extensionDevelopmentPath`
+            const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+            // The path to test runner
+            // Passed to --extensionTestsPath
+            const extensionTestsPath = path.resolve(__dirname, './suite/index');
+            // Download VS Code, unzip it and run the integration test
+            yield vscode_test_1.runTests({ extensionDevelopmentPath, extensionTestsPath });
+        }
+        catch (err) {
+            console.error('Failed to run tests');
+            process.exit(1);
+        }
+    });
+}
+main();
+//# sourceMappingURL=runTest.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,72 +1,72 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
-import * as fs from "fs";
-import translate from "@vitalets/google-translate-api";
+import * as vscode from 'vscode'
+import * as fs from 'fs'
+import translate from '@vitalets/google-translate-api'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "extension.md2urdu",
-    async () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World! Welcome to .md language converter to urdu"
-      );
-      // Getting information of current open file if that file is markdown get content of file
-      await checkFileType();
-    }
-  );
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand(
+        'extension.md2urdu',
+        async () => {
+            // The code you place here will be executed every time your command is executed
+            // Display a message box to the user
+            vscode.window.showInformationMessage(
+                'Hello World! Welcome to .md language converter to urdu'
+            )
+            // Getting information of current open file if that file is markdown get content of file
+            await checkFileType()
+        }
+    )
 
-  context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable)
 }
 
 // this method is called for checking current open file
 async function checkFileType() {
-  await Promise.all(
-    vscode.workspace.textDocuments.map(async file => {
-      const { languageId, fileName } = file;
-      if (languageId === "markdown") {
-        await getFileContent(fileName);
-      }
-    })
-  );
+    await Promise.all(
+        vscode.workspace.textDocuments.map(async file => {
+            const { languageId, fileName } = file
+            if (languageId === 'markdown') {
+                await getFileContent(fileName)
+            }
+        })
+    )
 }
 
 // this method is called for getting content of current file
 async function getFileContent(fileName: string) {
-  await fs.readFile(fileName, async (err, data) => {
-    if (!err) {
-      const fileData = await data.toString();
-      translate(fileData, { to: "ur" })
-        .then(async (res: any) => {
-          if (!res) throw Error("Unable to convert to urdu");
-          else {
-            const urduText = res.text;
-            await fs.writeFile(fileName, urduText, err => {
-              if (err) throw Error("Unable to convert to urdu");
-              vscode.window.showInformationMessage(
-                ".md Successfully Converted to urdu"
-              );
-            });
-          }
-        })
-        .catch((err: object) => {
-          throw Error("Unable to convert to urdu");
-        });
-    } else throw Error("Unable to convert to urdu");
-  });
+    await fs.readFile(fileName, async (err, data) => {
+        if (!err) {
+            const fileData = await data.toString()
+            translate(fileData, { to: 'ur' })
+                .then(async (res: any) => {
+                    if (!res) throw Error('Unable to convert to urdu')
+                    else {
+                        const urduText = res.text
+                        await fs.writeFile(fileName, urduText, err => {
+                            if (err) throw Error('Unable to convert to urdu')
+                            vscode.window.showInformationMessage(
+                                '.md Successfully Converted to urdu'
+                            )
+                        })
+                    }
+                })
+                .catch((err: object) => {
+                    throw Error('Unable to convert to urdu')
+                })
+        } else throw Error('Unable to convert to urdu')
+    })
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
-  vscode.window.showInformationMessage("md2urdu is deactivated");
+    vscode.window.showInformationMessage('md2urdu is deactivated')
 }
